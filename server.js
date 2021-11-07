7bd2efa44a50e29ae2d1d37a43380c2ad6ab5f5b
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-11-07 10:38:17 -0500

    commit2

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(express.static('public'));
 //   res.send(db);
 // });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
-// app.get('/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/notes.html'));
-// });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
 
 app.get('/api/notes', (req, res) => {
