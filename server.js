e323ad3c7444b8bc06f6b8390101e7e148ae6869
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-11-07 17:06:21 -0500

    edited code

--- server.js
+++ server.js
@@ -10,27 +10,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-
-
-
-//Get apiNotes
-app.get('/api/notes', (req, res) => {
-  res.json(db);
-});
-
-// Post apiNotes
-app.post('/api/notes', (req, res) => {
-const results = db;
-req.body.id = (results);
-db.push(req.body);
-fs.writeFileSync(path.join(__dirname, './db/db.json', JSON.stringify(db)));
-  
-
-res.json(db);
-});
-
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -38,6 +17,23 @@ app.get('*', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+
+
+//Get apiNotes
+app.get('/api/notes', (req, res) =>{
+  
+  res.json(db);
+});
+
+// Post apiNotes
+app.post('/api/notes', (req, res) =>{
+ 
+  
+ });
+
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
