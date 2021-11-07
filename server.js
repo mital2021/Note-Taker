0985b93afef2b82a972cd0abe61fdddedb0d4bbb
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-11-07 09:31:01 -0500

    second commit

--- server.js
+++ server.js
@@ -9,17 +9,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/db', (req, res) => {
-  res.send(db);
+// app.get('/api/db', (req, res) => {
+//   res.send(db);
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/index.html'));
-// });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
-// app.get('/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/notes.html'));
-// });
+
+app.post('/api/db', (req, res) => {
+console.log(req.body);
+res.join(req.body);
+});
+
+
+
+
+function findById(id, noteTaker) {
+  const result = noteTaker.filter(Note => Note.id === id)[0];
+  return result;
+}
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
