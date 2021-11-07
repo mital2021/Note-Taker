b1b22ef44077d467192e6524cff14e36ea6a0ba1
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-11-07 18:45:57 -0500

    draft1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const notecheck  = require('./db/db.json');
+const checknote  = require('./db/db.json');
 const express = require('express');
 const path = require('path');
 const app = express();
-//const util = require('util');
+const util = require('util');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
@@ -24,45 +24,21 @@ app.get('/notes', (req, res) => {
 //Get apiNotes
 app.get('/api/notes', (req, res) =>{
 
-  res.json(notecheck);
+  res.json(checknote);
 });
 
 
 
-function createNewNote(body, notesArray) {
-  const newNote = body;
-  if (!Array.isArray(notesArray))
-      notesArray = [];
-  
-  if (notesArray.length === 0)
-      notesArray.push(0);
-
-  body.id = notesArray[0];
-  notesArray[0]++;
-
-  notesArray.push(newNote);
-  fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(notesArray, null, 2)
-  );
-  return newNote;
-}
-
+ // Post apiNotes
 app.post('/api/notes', (req, res) => {
-  const newNote = createNewNote(req.body, allNotes);
-  res.json(newNote);
-});
-
-// // Post apiNotes
-// app.post('/api/notes', (req, res) => {
-// const id = req.body;
-//   req.body.id = id();
-// db.push(req.body);
-//   fs.writeFileAsy('./db/db.json', JSON.stringify(db), {
+const id = req;
+  req.body.id = id;
+checknote.push(req.body);
+  fs.writeFile('./db/db.json', JSON.stringify(checknote), {
       
-//   });
-//   res.json(db);
-// });
+  });
+  res.json(checknote);
+});
 
   
 
