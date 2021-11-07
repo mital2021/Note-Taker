20c246752715c04bf4fe4e5ac9de539c399e1cef
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-11-07 09:09:02 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const { db } = require('./db/db');
+const db  = require('./db/db.json');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/api/db', (req, res) => {
-  res.send('Hello!');
+  res.send(db);
 });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/index.html'));
+// });
+
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/notes.html'));
+// });
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+
+
